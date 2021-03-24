d28a31c235beac11f33bada7bc6fbb8dd02dd580
gisungchoi <chlrltjd159@naver.com>
2021-03-24 21:08:33 +0900

    fix : 중복 제거

--- src/main/resources/static/js/validForm.js
+++ src/main/resources/static/js/validForm.js
@@ -1,6 +1,7 @@
 var validateFlag = false;
 function validate() {
 
+    alert(validateFlag+"현상태");
     if(validateFlag == true) {
         return;
     }
@@ -13,9 +14,11 @@ function validate() {
         if (getInput[i].value.trim() == "") {
             let getName = getInput[i].getAttribute("id");
             alert(getName + "를 입력하세요")
+             alert(validateFlag+"현상태2");
             return;
         }
     }
     validateFlag = false;
     document.question.submit();
+             alert(validateFlag+"현상태3");
 }
